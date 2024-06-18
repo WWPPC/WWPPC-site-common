7ce0fbec6a2eba7a2ca8811663812b7d348d4055
Sampleprovider(sp) <radioactivestuff.101@gmail.com>
2024-06-18 12:05:18 -0400

    it didnt work

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,12 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import dts from 'vite-plugin-dts'
 import { resolve } from 'node:path'
-
-import tsconfig from './tsconfig.app.json'
-
-const entries: any = {}
-for (const file of tsconfig.files) {
-    const index = file.match(/.*\/(.*)/);
-    if (index != null) entries[index[1]] = resolve(__dirname, file)
-}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [vue(), dts()],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
@@ -22,7 +15,22 @@ export default defineConfig({
     },
     build: {
         lib: {
-            entry: entries,
+            entry: {
+                index: resolve(__dirname, 'src/index.ts'),
+                common: resolve(__dirname, 'src/common/common.ts'),
+                containers: resolve(__dirname, 'src/containers/containers.ts'),
+                inputs: resolve(__dirname, 'src/inputs/inputs.ts'),
+                text: resolve(__dirname, 'src/text/text.ts'),
+                modal: resolve(__dirname, 'src/modal/modal.ts'),
+                panels: resolve(__dirname, 'src/panels/panels.ts'),
+                userAgent: resolve(__dirname, 'src/scripts/userAgent.ts'),
+                recaptcha: resolve(__dirname, 'src/scripts/recaptcha.ts'),
+                katexify: resolve(__dirname, 'src/scripts/katexify.ts'),
+                title: resolve(__dirname, 'src/scripts/title.ts'),
+                serverConnection: resolve(__dirname, 'src/scripts/ServerConnection.ts'),
+                connectionEnforcer: resolve(__dirname, 'src/scripts/ConnectionEnforcer.ts'),
+                accountManager: resolve(__dirname, 'src/scripts/AccountManager.ts')
+            },
             name: 'WWPPCSiteCommon',
             formats: ['es']
         },
