210a2f7a7575d2ade44a4ab17d6491d03e5d600c
siddharthgoli <siddharth.goli@gmail.com>
2025-04-06 16:00:38 -0400

    Update SponsorList.vue

--- src/common-components/home/home/SponsorList.vue
+++ src/common-components/home/home/SponsorList.vue
@@ -1,38 +1,41 @@
 <script setup lang="ts">
-import { GlowText } from '#/text';
+import { GlitchText, GlowText } from '#/text';
 import HomeSponsorLogo from '#/common-components/home/home/HomeSponsorLogo.vue';
+import LineDivider from '#/common/LineDivider.vue';
+
+type homeSponsorLogo = { name: string, src: string, url: string, height: string }
 
 defineProps<{
-    partners: { name: string; src: string; url: string; height: string }[];
-    gold: { name: string; src: string; url: string; height: string }[];
-    silver: { name: string; src: string; url: string; height: string }[];
-    otherSponsors: { name: string; src: string; url: string; height: string }[];
+    color: string
+    partners: homeSponsorLogo[]
+    gold: homeSponsorLogo[]
+    silver: homeSponsorLogo[]
+    otherSponsors: homeSponsorLogo[]
 }>();
-
 </script>
 
 <template>
     <div class="centered">
-            <GlitchText text="Sponsors" font-size="var(--font-title)" color="lime" glow shadow random :steps=2 on-visible></GlitchText>
+            <GlitchText text="Sponsors" font-size="var(--font-title)" :color=$props.color glow shadow random :steps=2 on-visible></GlitchText>
         </div>
     <div class="sponsorColumns">
-        <div>
-            <GlowText text="PARTNERS" font-size="var(--font-huge)" color="lime" shadow></GlowText>
+        <div v-if="partners.length != 0">
+            <GlowText text="PARTNERS" font-size="var(--font-huge)" :color=$props.color shadow></GlowText>
             <HomeSponsorLogo v-for="sponsor in partners" :key="sponsor.name" :src="sponsor.src" :url="sponsor.url" :name="sponsor.name" :height="sponsor.height" />
         </div>
-        <div>
+        <div v-if="gold.length != 0">
             <GlowText text="GOLD" font-size="var(--font-huge)" color="#FD0" shadow></GlowText>
             <HomeSponsorLogo v-for="sponsor in gold" :key="sponsor.name" :src="sponsor.src" :url="sponsor.url" :name="sponsor.name" :height="sponsor.height" />
         </div>
-        <div>
+        <div v-if="silver.length != 0">
             <GlowText text="SILVER" font-size="var(--font-huge)" color="#CCC" shadow></GlowText>
             <HomeSponsorLogo v-for="sponsor in silver" :key="sponsor.name" :src="sponsor.src" :url="sponsor.url" :name="sponsor.name" :height="sponsor.height" />
         </div>
     </div>
 
-    <LineDivider color="#AAA"></LineDivider>
+    <LineDivider v-if="otherSponsors.length != 0" color="#AAA"></LineDivider>
 
-    <div class="sponsors">
+    <div class="sponsors" v-if="otherSponsors.length != 0">
         <HomeSponsorLogo v-for="sponsor in otherSponsors" :key="sponsor.name" :src="sponsor.src" :url="sponsor.url" :name="sponsor.name" :height="sponsor.height" />
     </div>
 </template>
@@ -47,18 +50,20 @@ defineProps<{
     padding: 0px 5%;
 }
 
-.sponsorColumns > div {
+.sponsorColumns>div {
     width: 40vh;
     display: flex;
     flex-direction: column;
     align-items: center;
 }
 
-.sponsor-logos {
+.sponsors {
     display: flex;
+    flex-direction: row;
     flex-wrap: wrap;
     justify-content: center;
-    gap: 20px;
-    margin-top: 10px;
+    row-gap: 25px;
+    column-gap: 50px;
+    padding: 0px 5%;
 }
 </style>
